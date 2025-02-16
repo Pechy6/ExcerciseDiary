0fc91d0283e5d28e7f863fd3a6a783ff6b05983d
Pechy6 <150392896+Pechy6@users.noreply.github.com>
2025-02-16 15:03:17 +0100

    Add basic diary application with a record management system
    Implemented a console-based diary application in C# with functionality to add, view, navigate, and delete records. Included solution structure, project files, and a `.gitignore` for managing build directories and IDE-specific files.

---
+++ Diary/Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Diary;
+
+Zaznam zaznam = new Zaznam();
+
+void VytiskniMenu()
+{
+    Console.Clear();
+
+    Console.WriteLine("-------------------------------------------------------");
+    Console.WriteLine("Deník se ovládá následujícími příkazy:");
+    Console.WriteLine("- predchozi: Přesunutí na předchozí záznam");
+    Console.WriteLine("- dalsi: Přesunutí na další záznam");
+    Console.WriteLine("- novy: Vytvoření nového záznamu");
+    Console.WriteLine("- uloz: Uložení vytvořeného záznamu");
+    Console.WriteLine("- smaz: Odstranění záznamu");
+    Console.WriteLine("- zavri: Zavření deníku");
+    Console.WriteLine("-------------------------------------------------------\n");
+}
+
+
+string prechozi = "predchozi";
+string dalsi = "dalsi";
+string novy = "novy";
+string uloz = "uloz";
+string smaz = "smaz";
+string zavri = "zavri";
+int pocetZaznamu = 0;
+bool ukazZaznam = false;
+
+while (true)
+{
+    VytiskniMenu();
+    Console.WriteLine($"Pocet zaznamu: {pocetZaznamu}\n");
+    if (ukazZaznam)
+    {
+        if (zaznam != null)
+        {
+            zaznam.ZobrazAktualniZaznam();
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------\n");
+        }
+    }
+
+    string vstup = Console.ReadLine().Trim().ToLower();
+
+
+    if (vstup == zavri)
+    {
+        Console.WriteLine("Dekujeme za pouzivani aplikace.");
+        break;
+    }
+
+    if (vstup == prechozi)
+    {
+        zaznam.PredchoziZaznam();
+    }
+
+    else if (vstup == dalsi)
+    {
+        zaznam.DalsiZaznam();
+    }
+
+    else if (vstup == novy)
+    {
+        Console.Clear();
+        VytiskniMenu();
+        DateTime datum;
+        Console.Write($"Zadej datum: ");
+        while (!DateTime.TryParse(Console.ReadLine(), out datum))
+            Console.Write("Zadal jste spatny format.");
+
+        StringBuilder text = new StringBuilder();
+        Console.WriteLine($"Text:");
+        while (true)
+        {
+            string line = Console.ReadLine();
+            // pokud uzivatel zada uloz ukonci smycku
+            if (line.Trim().ToLower() == uloz) break;
+
+            // prida text do promene text
+            text.Append(line);
+        }
+
+        zaznam.PridejZaznam(datum, text.ToString());
+        pocetZaznamu++;
+
+        if (vstup == uloz)
+        {
+            zaznam.PridejZaznam(datum, text.ToString());
+        }
+
+        ukazZaznam = true;
+    }
+
+    if (vstup == smaz)
+    {
+        Console.Clear();
+        VytiskniMenu();
+        Console.WriteLine($"\nPro odstranění tohoto záznamu stiskni \"a\", pro zrušení jiný znak.");
+        char znakProPotvrzeni = Console.ReadKey().KeyChar;
+        if (znakProPotvrzeni == 'a')
+        {
+            zaznam.VymazZaznam();
+            if (pocetZaznamu > 0)
+            {
+                pocetZaznamu--;
+            }
+        }
+        else
+            continue;
+    }
+}
---
+++ Diary/Zaznam.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+
+namespace Diary;
+
+public class Zaznam
+{
+    DateTime Datum { get; set; }
+    string Text { get; set; }
+
+    public LinkedListNode<Zaznam> Node { get; set; }
+    public LinkedList<Zaznam> Zaznamy { get; set; } = new();
+
+
+    public Zaznam(DateTime datum, string text)
+    {
+        Datum = datum;
+        Text = text;
+    }
+
+    public Zaznam()
+    {
+    }
+
+    public void PridejZaznam(DateTime datum, string text)
+    {
+        var novyZaznam = new Zaznam(datum, text);
+        Zaznamy.AddLast(novyZaznam);
+
+        if (Node == null)
+        {
+            Node = Zaznamy.First;
+        }
+    }
+
+    public void VymazZaznam()
+    {
+        if (Node == null)
+        {
+            Console.WriteLine("Zaznamy nejsou k dispozici.");
+            return;
+        }
+
+        // Vybere nasledujici nebo predchozi uzel
+        var dalsiNode = Node.Next != null ? Node.Next : Node.Previous;
+
+        // odstrani aktualni uzel ze zaznamu
+        Zaznamy.Remove(Node);
+
+        // nastavi dalsi uzel jako aktualni
+        Node = dalsiNode;
+    }
+
+    public void DalsiZaznam()
+    {
+        if (Node == null)
+        {
+            Console.WriteLine("Zaznamy nejsou k dispozici.");
+        }
+
+        if (Node.Next != null)
+        {
+            Node = Node.Next;
+            Console.WriteLine($"Datum: {Node.Value.Datum}");
+            Console.WriteLine($"Text: {Node.Value.Text}");
+        }
+        else
+        {
+            Console.WriteLine("Nebyl nalezen dalsi zaznam.");
+        }
+    }
+
+    public void PredchoziZaznam()
+    {
+        if (Node == null)
+        {
+            Console.WriteLine("Zaznamy nejsou k dispozici.");
+        }
+
+        if (Node.Previous != null)
+        {
+            Node = Node.Previous;
+            Console.WriteLine($"Datum: {Node.Value.Datum}");
+            Console.WriteLine($"Text: {Node.Value.Text}");
+        }
+        else
+        {
+            Console.WriteLine("Nebyl nalezen predchozi zaznam.");
+        }
+    }
+
+    public void ZobrazAktualniZaznam()
+    {
+        if (Node == null)
+        {
+            Console.WriteLine("Zaznamy nejsou k dispozici.");
+            return;
+        }
+
+        Console.WriteLine($"Datum: {Node.Value.Datum:d}\n");
+        foreach (var text in Node.Value.Text)
+        {
+            Console.Write($"{text}");
+        }
+    }
+}
